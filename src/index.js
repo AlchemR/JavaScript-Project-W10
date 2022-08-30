c820564a2590165cdeaf3d9dc8f85c4a18bca518
AlchemR <102183958+AlchemR@users.noreply.github.com>
2022-08-30 01:58:22 -0700

    intro finished

--- src/index.js
+++ src/index.js
@@ -22,8 +22,13 @@ window.onload = function () {
   const intro3 = document.querySelector('#intro3')
   const intro3button3 = document.querySelector('#intro3button-3')
   const intro3button = document.querySelector('#intro3button')
+
   const intro4 = document.querySelector('#intro4')
   const intro4button = document.querySelector('#intro4button')
+  const intro4button4 = document.querySelector('#intro4button-4')
+ 
+  const intro5 = document.querySelector('#intro5')
+  const intro5button = document.querySelector('#intro5button')
 
   const startbutton = document.querySelector('#startbutton')
   const level1flag = document.querySelector('#level1flag')
@@ -42,16 +47,19 @@ window.onload = function () {
   intro2button.addEventListener('click', () => { ; intro2.style.display = 'none'; intro3.style.display = 'block'; intro3button.style.display = 'block'; intro2.classList.remove("flip-card-active"); intro3button3.style.display = 'block'  })
 
   intro3button3.addEventListener('click', () => { ; intro3button3.style.display = 'none'; intro3button.style.display = 'block'; intro3.classList.add("flip-card-active") })
-  intro3button.addEventListener('click', () => { ; intro3.style.display = 'none'; intro4.style.display = 'block'; intro4button.style.display = 'block';  })
-  intro4button.addEventListener('click', () => { ; intro4button.style.display = 'none'; level1flag.style.display = 'block'; startbutton.style.display = 'block'; intro4.classList.add("flip-card-active"); })
+  intro3button.addEventListener('click', () => { ; intro3.style.display = 'none'; intro4.style.display = 'block'; intro4button4.style.display = 'block';  })
   
-  startbutton.addEventListener('click', () => { level1(); startbutton.style.display = 'none'; level1flag.style.display = 'none'; intro4.style.display = "none" })
+  intro4button4.addEventListener('click', () => { ; intro4button4.style.display = 'none'; intro4button.style.display = 'block'; intro4.classList.add("flip-card-active") })
+  intro4button.addEventListener('click', () => { ; intro4.style.display = 'none'; intro5.style.display = 'block'; intro5button.style.display = 'block';  })
+  intro5button.addEventListener('click', () => { ; intro5button.style.display = 'none'; level1flag.style.display = 'block'; startbutton.style.display = 'block'; intro5.classList.add("flip-card-active"); })
+  
+  startbutton.addEventListener('click', () => { level1(); startbutton.style.display = 'none'; level1flag.style.display = 'none'; intro5.style.display = "none" })
   startbutton2.addEventListener('click', () => { level2(); startbutton2.style.display = 'none'; level2flag.style.display = 'none'; })
   startbutton3.addEventListener('click', () => { level3(); startbutton3.style.display = 'none'; level3flag.style.display = 'none' })
   startbutton4.addEventListener('click', () => { level4(); startbutton4.style.display = 'none'; level4flag.style.display = 'none' })
   canvas1.height = 100
-  if (innerWidth > 1200) {innerWidth = 1200}
-  if (innerHeight > 780) {innerHeight = 780}
+  // if (innerWidth > 1200) {innerWidth = 1200}
+  // if (innerHeight > 780) {innerHeight = 780}
   // if (innerWidth > 1440) {innerWidth = 1440}
   canvas1.width = innerWidth
   window.context2 = context2
